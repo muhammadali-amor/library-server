df856eefa5397556f0d9e32c75f841057e7033d5
Muhammadali <132482516+muhammadali-amor@users.noreply.github.com>
2025-02-28 00:03:38 +0500

    nastroy

---
+++ src/main/java/it/library_server/component/DataLoader.java
@@ -0,0 +1,65 @@
+package it.library_server.component;
+
+import ali.school_server.entity.About;
+import ali.school_server.entity.Role;
+import ali.school_server.entity.User;
+import ali.school_server.entity.enums.RoleName;
+import ali.school_server.repository.AboutRepository;
+import ali.school_server.repository.AuthRepository;
+import ali.school_server.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.UUID;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner {
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String intiMode;
+
+    private final AuthRepository authRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AboutRepository aboutRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (intiMode.equals("create") || intiMode.equals("create-drop")) {
+            for (RoleName value : RoleName.values()) {
+                roleRepository.save(new Role(value));
+            }
+            authRepository.save(
+                    User.builder()
+                            .name("Admin")
+                            .surname("Adminov")
+                            .phoneNumber("987654321")
+                            .email("school@gmail.com")
+                            .password(passwordEncoder.encode("root1234"))
+                            .roles(new HashSet<>(roleRepository.findAll()))
+                            .accountNonLocked(true)
+                            .accountNonExpired(true)
+                            .credentialsNonExpired(true)
+                            .enabled(true)
+                            .build()
+            );
+        }
+
+        About about = About.builder()
+                .title("School name")
+                .description("School description")
+                .photoId(UUID.randomUUID())
+                .build();
+        about.setName("School name");
+
+        aboutRepository.save(
+                about
+        );
+    }
+}
